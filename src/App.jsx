deaa63a544fe782d1f63c3a8bc5c139e77e374bc
Hezelnat30 <justin.hezekiel123@gmail.com>
2024-05-21 20:58:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold text-teal-600 text-center uppercase">
+        Hello from react!
+      </h1>
+    </>
+  );
+}
+
+export default App;
